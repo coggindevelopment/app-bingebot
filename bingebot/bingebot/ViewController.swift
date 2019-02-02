68fe47dc4ff6e361405df9da411cd87ff8ac888e
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-02 10:53:10 -0600

    lesson-03 commit -- BingeBot - Adding Shows to BingeBot

--- bingebot/bingebot/ViewController.swift
+++ bingebot/bingebot/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showsLabel: UILabel!
+    @IBOutlet weak var randomShowLabel: UILabel!
+    @IBOutlet weak var bingebotSpokenLabel: UILabel!
+    @IBOutlet weak var addShowTextField: UITextField!
+    @IBOutlet weak var addShowBtn: UIButton!
+    @IBOutlet weak var randomShowStackView: UIStackView!
+    @IBOutlet weak var addShowStackView: UIStackView!
+    @IBOutlet weak var showsStackView: UIStackView!
+    
+    var shows: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showsStackView.isHidden = true
+        randomShowStackView.isHidden = true
+    }
+    
+    func updateShowsLabel() {
+        showsLabel.text = shows.joined(separator: ", ")
     }
 
-
+    @IBAction func addShowBtnWasPressed(_ sender: Any) {
+        guard let showName = addShowTextField.text else { return }
+        shows.append(showName)
+        updateShowsLabel()
+        addShowTextField.text = ""
+        showsStackView.isHidden = false
+        
+        if shows.count > 1 {
+            randomShowStackView.isHidden = false
+            bingebotSpokenLabel.isHidden = true
+            randomShowLabel.isHidden = true
+        }
+    }
 }
 
